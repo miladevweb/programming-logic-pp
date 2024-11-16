ebb323b399b247b696496bb7fa4060f3fdf1196b
miladevweb <miladevweb@outlook.com>
2024-11-16 10:37:29 -0500

    docs: modify index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 function strStr(haystack: string, needle: string): number {
   // Si la subcadena needle está vacía, devolver 0
-  if (needle === '') {
-    return 0
-  }
+  if (needle === '') return 0
 
   // Buscar la primera aparición de needle en haystack
   return haystack.indexOf(needle)
