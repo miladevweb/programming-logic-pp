731a9d39122cf926e01ad9431b5712ba17112029
miladevweb <miladevweb@outlook.com>
2024-11-27 15:03:36 -0500

    docs: add second result

--- src/practice.ts
+++ src/practice.ts
@@ -12,7 +12,26 @@ function isValidPractice(s: string) {
       let last = stack.pop()
       if (last && pairs[last] !== char) return 'invalid'
     }
-
-    return stack.length === 0 ? 'valid' : 'invalid'
   }
+  return stack.length === 0 ? 'valid' : 'invalid'
+}
+
+function run(s: string) {
+  const stack: string[] = []
+  const pairs: { [key: string]: string } = {
+    '}': '{',
+    ')': '(',
+    ']': '[',
+  }
+
+  for (let char of s) {
+    if (pairs[char]) {
+      let last = stack.pop()
+      if (last !== pairs[char]) return 'invalid'
+    } else {
+      stack.push(char)
+    }
+  }
+
+  return stack.length === 0 ? 'valid' : 'invalid'
 }
