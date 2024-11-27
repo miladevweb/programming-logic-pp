e95dcb0dc17b95b9b4f9d58e433f9634bfdc294f
miladevweb <miladevweb@outlook.com>
2024-11-27 16:34:23 -0500

    docs: add run function

--- src/practice.ts
+++ src/practice.ts
@@ -1,4 +1,4 @@
-function mergePractice(nums1: number[], m: number, nums2: number[], n: number) {
+function mergePractice(nums1: number[], m: number, nums2: number[], n: number): void {
   let i = m - 1 // It points the last valid index in nums1
   let j = n - 1 // It points the last index in nums2
   let k = m + n - 1 // It points the last index in nums1
@@ -17,6 +17,32 @@ function mergePractice(nums1: number[], m: number, nums2: number[], n: number) {
   }
 
   // If there are elements left in nums2: O(n)
+  while (j >= 0) {
+    nums1[k] = nums2[j]
+    j--
+    k--
+  }
+}
+
+// Merge nums1 and nums2 into nums1 - non-decreasing order
+function run(nums1: number[], m: number, nums2: number[], n: number): void {
+  let i = m - 1 // Pointer to the last valid index in nums1
+  let j = n - 1 // Pointer to the last index in nums2
+  let k = m + n - 1 // Pointer to the last index in nums1
+
+  // While nums1 and nums2 have elements....
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] >= nums2[j]) {
+      nums1[k] = nums1[i]
+      i--
+    } else {
+      nums1[k] = nums2[j]
+      j--
+    }
+    k--
+  }
+
+  // While nums2 has elements....
   while (j >= 0) {
     nums1[k] = nums2[j]
     j--
