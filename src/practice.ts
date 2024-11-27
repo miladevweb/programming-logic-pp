afc8c48979ed8e1b78d80e6968031e00c5234c98
miladevweb <miladevweb@outlook.com>
2024-11-27 15:54:20 -0500

    docs: add run function

--- src/practice.ts
+++ src/practice.ts
@@ -1,10 +1,25 @@
+// Delete all val elements in nums
 function removeElementPractice(nums: number[], val: number) {
-  let k = 0 // Number of elements that are not equal to val
+  let k = 0 // Pointer that tracks which elements are valid
 
   // O(n)
   for (let n of nums) {
     if (n !== val) {
       nums[k] = n
+      k++
+    }
+  }
+
+  return k
+}
+// Return the number of elements that are not equal to val
+
+function run(nums: number[], val: number) {
+  let k = 0 // Pointer that tracks which elements are different from val
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k] = nums[i]
       k++
     }
   }
