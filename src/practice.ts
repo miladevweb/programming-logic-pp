f947e0cfb4576b4a568ba922924d6b912381c972
miladevweb <miladevweb@outlook.com>
2024-11-29 16:36:23 -0500

    docs: add comments and logs

---
+++ src/practice.ts
@@ -0,0 +1,20 @@
+function run(prices: number[]): number {
+  let minPrice = Infinity // it points the minimum price so far, this pointer help us to calculate the maximum profit
+  let maxProfit = 0
+
+  for (let price of prices) {
+    // update lowest price
+    if (price < minPrice) minPrice = price
+
+    // calculate profit
+    const profit = price - minPrice
+
+    // update maxProfit only if it's greater
+    if (profit > maxProfit) maxProfit = profit
+    console.log({ price, profit, minPrice, maxProfit })
+  }
+
+  return maxProfit
+}
+
+console.log(run([5, 4, 10, 2, 1]))
