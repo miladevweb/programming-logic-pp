b6c3d690609e2a3c0c290b7290cae0ca086fc565
miladevweb <miladevweb@outlook.com>
2024-12-03 10:33:39 -0500

    docs: add explanation

--- src/practice.ts
+++ src/practice.ts
@@ -15,7 +15,7 @@ function removeDuplicatesPractice(nums: number[]) {
 
 // nums is a non-decreasing array
 // remove elements in-place such that each element is unique
-function rn(nums: number[]): number {
+function run(nums: number[]): number {
   if (nums.length === 0) return 0
   let k = 1
 
@@ -27,4 +27,18 @@ function rn(nums: number[]): number {
   }
 
   return k
+}
+
+// non-decreasing => [1,1,1,4,5,5]
+function execute(nums: number[]) {
+  if (nums.length === 0) return 0
+  let k = 1 // the first element is always unique
+
+  // it iterates from second value
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[i - 1]) {
+      nums[k] = nums[i]
+      k++
+    }
+  }
 }
