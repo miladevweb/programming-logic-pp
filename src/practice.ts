8d1f073c025561f5ab2bbb840a303bd32a174f30
miladevweb <miladevweb@outlook.com>
2024-12-03 09:56:01 -0500

    docs: add description

--- src/practice.ts
+++ src/practice.ts
@@ -1,15 +1,16 @@
+// [1, 2, 3, 4, 5] - 2 => [4, 5, 1, 2 ,3]
 function run(nums: number[], k: number): number[] {
   const n = nums.length
   k = k % n // if k is greater than n
 
   // Step 1: Revert the entire array
-  reverse(nums, 0, n - 1)
+  reverse(nums, 0, n - 1) // [5, 4, 3, 2, 1]
 
   // Step 2: Revert first k elements
-  reverse(nums, 0, k - 1)
+  reverse(nums, 0, k - 1) // [4, 5, 3, 2, 1]
 
   //   // Step 3: Revert the remaining elements
-  reverse(nums, k, n - 1)
+  reverse(nums, k, n - 1) // [4, 5, 1, 2, 3]
   return nums
 }
 
@@ -26,9 +27,9 @@ function execute(nums: number[], k: number): number[] {
   k = k % n // If k is greater than n, we reduce it to an effective value
   if (k === 0) return nums
 
-  const rightPart = nums.slice(-k) // Last k elements
-  const leftPart = nums.slice(0, n - k) // First n - k elements
+  const leftPart = nums.slice(-k) // Last k elements
+  const rightPart = nums.slice(0, n - k) // First n - k elements
 
-  nums.splice(0, n, ...rightPart, ...leftPart) // remove n elements from index 0 and replace them with right and left part destructured values
+  nums.splice(0, n, ...leftPart, ...rightPart) // remove n elements from index 0 and replace them with right and left part destructured values, we don't need what it returns
   return nums
 }
