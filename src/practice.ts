d94ad82df6e155c4f279ad98846b027a87f9722c
miladevweb <miladevweb@outlook.com>
2024-12-12 11:50:39 -0500

    docs: add execute function

--- src/practice.ts
+++ src/practice.ts
@@ -1,3 +1,28 @@
+function execute(nums1: number[], m: number, nums2: number[], n: number): number[] {
+  let i = m - 1 // Points the last valid element of nums1
+  let j = n - 1 // Points the last element of nums2
+  let k = m + n - 1 // Points the last element of nums1
+
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] > nums2[j]) {
+      nums1[k] = nums1[i]
+      i--
+    } else {
+      nums1[k] = nums2[j]
+      j--
+    }
+    k--
+  }
+
+  while (j >= 0) {
+    nums1[k] = nums2[j]
+    j--
+    k--
+  }
+
+  return nums1
+}
+
 function mergePractice(nums1: number[], m: number, nums2: number[], n: number): void {
   let i = m - 1 // It points the last valid index in nums1
   let j = n - 1 // It points the last index in nums2
