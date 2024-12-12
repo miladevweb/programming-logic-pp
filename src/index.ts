911da5914d158e6a1d515ed7fe0fcf62f9a6ad6b
miladevweb <miladevweb@outlook.com>
2024-12-12 14:28:24 -0500

    docs: add comments

--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,17 @@ function hIndex(citations: number[]): number {
 
   return h
 }
+
+// [1, 2, 3, 4, 5] has 5 articles and each element is a number of citations for that article
+function run(citations: number[]): number {
+  citations.sort((a, b) => b - a) // order in descending order
+
+  let h = 0
+  for (let i = 0; i < citations.length; i++) {
+    // número mínimo de citas que debe tener cada articulo
+    if (citations[i] >= i + 1) h = i + 1
+    else break
+  }
+
+  return h
+}
